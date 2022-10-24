fd6ae0fa9579569dd2917d3558c82c73402880de
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-10-24 18:48:12 +0200

    Baraja creada

---
+++ main.py
@@ -0,0 +1,19 @@
+# BLACJACK
+# Creo la baraja de poker con las puntuaciones de cada carta
+
+cartas = {
+    chr(0x1f0a1): 11,
+    chr(0x1f0a2): 2,
+    chr(0x1f0a3): 3,
+    chr(0x1f0a4): 4,
+    chr(0x1f0a5): 5,
+    chr(0x1f0a6): 6,
+    chr(0x1f0a7): 7,
+    chr(0x1f0a8): 8,
+    chr(0x1f0a9): 9,
+    chr(0x1f0a0): 10,
+    chr(0x1f0aa): 10,
+    chr(0x1f0ab): 10,
+    chr(0x1f0ac): 10
+
+}
